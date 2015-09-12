5e4cbbad15c3a202eac3f29f929549d0c11e572f
Sergey Travin <sergey.a.travin@gmail.com>
2015-09-13 01:12:08 +0600

    Init

---
+++ ClassLibrary2/LogAnalyzerTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using LogAn;
+using System;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        [Test]
+        public void IsValidLogFileName_BadExtension_ReturnsFalse()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            bool result = analyzer.IsValidLogFileName("filewithbadextension.foo");
+            Assert.False(result);
+        }
+    }
+}
